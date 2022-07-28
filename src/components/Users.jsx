93ca2b6e92feb607f9e27974ab9815265c31505f
kelvynloreto <loretokelvyn@gmail.com>
2022-07-27 22:25:33 -0400

    .?

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -4,17 +4,17 @@ export const Users = ({user}) => {
   return (
     <div className="card_users">
       <h2>
-        Mi nombre es:
-        {`${user?.name.title} ${user?.name.first} ${user?.name.last}`}
+        Name: 
+        {` ${user?.name.title} ${user?.name.first} ${user?.name.last}`}
       </h2>
       <img src={user?.picture.large} alt="" />
-      <h3>Mi correo es: {user?.email}</h3>
-      <h3>Genero: {user?.gender}</h3>
+      <h3>Email: {user?.email}</h3>
+      <h3>Gender: {user?.gender}</h3>
       <h3>
-        Soy de: {user?.location.country} vivo en {user?.location.state} en la
-        ciudad {user?.location.city}
+        Country: {user?.location.country} , State: {user?.location.state} , 
+        City: {user?.location.city}
       </h3>
-      <h3>Mi Numero de telefono es: {user?.cell}</h3>
+      <h3>Number: {user?.cell}</h3>
     </div>
   );
 };
